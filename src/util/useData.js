0d86de6635f902321bbc7c1456d20f65f25e1e72
Michael Stiffler <michael.r.stiffler@sherwin.com>
2024-12-10 11:07:31 -0500

    Update useData() to sort by shop

--- src/util/useData.js
+++ src/util/useData.js
@@ -10,9 +10,21 @@ export function useData () {
                     header: true,
                     complete: (results) => {
                         let resultArr = results.data;
+                        let mockShop1 = {name: "Mock Shop 1", results: []};
+                        let mockShop2 = {name: "Mock Shop 2", results: []};
+                        let mockShop3 = {name: "Mock Shop 3", results: []};
                         resultArr.forEach((row) => {
-                            filteredArr.push(row);
+                            if(row["SHOP_NAME"] === "Mock Shop 1"){
+                                mockShop1.results.push(row)
+                            } else if (row["SHOP_NAME"] === "Mock Shop 2") {
+                                mockShop2.results.push(row)
+                            } else {
+                                mockShop3.results.push(row)
+                            }
                         });
+                        filteredArr.push(mockShop1);
+                        filteredArr.push(mockShop2);
+                        filteredArr.push(mockShop3);
                         console.log("NATES ARRAY", filteredArr);
                     },
                 });
